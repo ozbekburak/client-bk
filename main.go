318ef8d593e2975f9aefe3773b4275ed49a25128
ozbekburak <ozbekburakkaan@outlook.com>
2021-07-02 09:09:33 +0300

    create get request

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello from our http client")
+	Get("https://www.google.com")
+}
+
+func Get(url string) {
+	c := http.Client{
+		Timeout: time.Duration(1) * time.Second}
+	response, err := c.Get(url)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+	fmt.Printf("Body: %s", body)
+}
