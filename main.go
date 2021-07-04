e35069de17645ecc36ad98bbd25076d80df5713d
Burak Kaan Özbek <ozbekburakkaan@outlook.com>
2021-07-04 20:45:26 +0300

    complete put and some cleaning

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
@@ -14,10 +13,42 @@ type Mentor struct {
 	Surname string `json:"surname"`
 }
 
+// PostData güzel bir isimlendirme değil gibi ama Post() fonksiyon ismiyle çakışıyor
+type PostData struct {
+	UserID int    `json:"userId"`
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+}
+
 func main() {
 	fmt.Println("Burak Kaan HTTP Client")
-	Get("https://jsonplaceholder.typicode.com/posts/1")
 	Post("https://postman-echo.com/post")
+	Get("https://jsonplaceholder.typicode.com/posts/1")
+	Put("https://jsonplaceholder.typicode.com/posts/2")
+}
+
+
+func Post(url string) {
+	myMentor := Mentor{
+		"Erhan",
+		"Yakut",
+	}
+	data, _ := json.Marshal(myMentor)
+	requestBody := bytes.NewReader(data)
+
+	resp, err := http.Post(url, "application/json", requestBody)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+
+	fmt.Printf("\nResponse of POST request: %s", string(body))
 }
 
 func Get(url string) {
@@ -27,34 +58,48 @@ func Get(url string) {
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body) // byte döner. bodyBytes daha iyi bir adlandırma mı olur?
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Printf("Error: %s", err)
 	}
 
 	// string(body) ayrı bir değişkene atamak overkill mi best practice mi?
-	fmt.Printf("Get request response body: %s", string(body))
+	//fmt.Printf("Get request response body: %s", string(body))
+
+	var newPostData PostData
+	json.Unmarshal(body, &newPostData)
+	fmt.Printf("\nResponse of GET request:\n%+v\n", newPostData) // +v fieldları da yazıyor.
 }
 
-func Post(url string) {
-	m := Mentor{
-		"Erhan",
-		"Yakut",
+func Put(url string){
+	postData := PostData{
+		5,
+		2,
+		"Update the second element of posts",
+		"This is our new body for posts/2",
 	}
-	postBody, _ := json.Marshal(m)
-	requestBody := bytes.NewReader(postBody)
 
-	resp, err := http.Post(url, "application/json", requestBody)
+	client := &http.Client{}
+	data, _ := json.Marshal(postData)
+	requestBody := bytes.NewBuffer(data)
+
+	req, err := http.NewRequest(http.MethodPut, url, requestBody)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
+		fmt.Printf("Error: %s", err)
 	}
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
 
-
-	body, err := ioutil.ReadAll(resp.Body) // byte döner
+	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
+		fmt.Printf("Error: %s", err)
 	}
+
 	defer resp.Body.Close()
 
-	fmt.Printf("\nPost request response body: %s", string(body))
+	body, _ := ioutil.ReadAll(resp.Body) // body = byte[].
+
+	// Convert to PostData struct, string olarak da yazdırabilirdik. string(body).
+	var newPostData PostData
+	json.Unmarshal(body, &newPostData)
+	fmt.Printf("\nResponse of PUT request::\n%+v\n", newPostData) // +v fieldları da yazıyor.
 }
